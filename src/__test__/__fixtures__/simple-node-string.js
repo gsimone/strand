3572afcda1992a36faaa9fa1a4c2e072e11b9e83
emmelleppi <mlp.perego@gmail.com>
2020-11-30 15:18:23 +0100

    Adds fixtures

---
+++ src/__test__/__fixtures__/simple-node-string.js
@@ -0,0 +1,16 @@
+
+export default `
+{
+    "nodes": {
+        "black-quail-45": {
+        "name": "black-quail-45",
+        "fields": []
+        }
+    },
+    "positions": {
+        "black-quail-45": [100, 100]
+    },
+    "fields": {},
+    "connections": []
+}
+`
